006503bed069b596ce62c8f7036142657195b4b1
hiroakiitou <pigaru.gooood.mar19@gmail.com>
2021-02-05 17:23:28 +0900

    自動入力機能を追加

--- background.js
+++ background.js
@@ -1,21 +1,38 @@
-// 拡張機能アイコンクリック時にAmazonサイトを開く初期処理
+// 自動入力する項目と値を設定   
+var asinCode = "B01AFS9Q38/";                   //(任意)ASIN(Amazon商品ページのコード)  
+var params = { command: 'inputAddress',          //※処理に使う値の為固定
+               countryCode: "JP",                //(任意)国/地域 ※日本の場合固定
+               userName: "甘存 太郎",             //(必須)氏名 
+               postalCodeTop: "153",             //(必須)郵便番号上3桁
+               postalCodeUnder: "0064",          //(必須)郵便番号下4桁
+               region: "東京都",                  //(必須)都道府県
+               addrress1: "目黒区下目黒",         //(必須)住所1
+               addrress2: "1丁目8番1号",          //(任意)住所2
+               addrress3: "Amazon.co.jp",        //(任意)会社名
+               tell: "01234567890" };             //(必須)電話番号
+
+// 拡張機能アイコンクリック時に新規タブでAmazonサイトを開く初期処理
 chrome.browserAction.onClicked.addListener(function(activeTab){
-    // ↓今はAmazonのトップページを開くだけになっているが、キーワードを渡して商品を検索した状態のページを直接開くことも可能
-    // var keyword = "鬼滅の刃"
-    // var newURL = "https://www.amazon.co.jp/s/?field-keywords=" + keyword;
-    var newURL = "https://www.amazon.co.jp/"
-    chrome.tabs.create({ selected: true,
-                         url: newURL 
-    });
+    var newURL = "https://www.amazon.co.jp/gp/product/" + asinCode;
+    chrome.tabs.create({ selected: true, url: newURL });
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
-    // 読み込み時（loading）と読み込み完了時（complete）の内、
-    // 読み込み完了時のみ実行する。
-    if (info.status==='complete') {
-        // アクティブタブがAmazonのURLならContent.jsの処理を実行する。
-        if (tab.url.match("https://www.amazon.co.jp/*")!==null){
-            chrome.tabs.sendMessage(tabId, {command: 'update'});
-        }
+// 右クリックメニューの定義
+chrome.runtime.onInstalled.addListener(function(){
+    chrome.contextMenus.create({
+        id: "autoInput",
+        title: "Amazon住所自動入力",
+        contexts: ["all"],
+        type: "normal",
+    })
+})
+//右クリックメニューから「Amazon住所自動入力」がクリックされた時のコード
+chrome.contextMenus.onClicked.addListener(function(event) {
+    if(event.menuItemId === "autoInput"){
+        chrome.tabs.query({ active: true, lastFocusedWindow:true }, tabs => {
+            if (tabs[0].url.match("https://www.amazon.co.jp/*")!==null){
+                chrome.tabs.sendMessage(tabs[0].id, params);
+            }
+        });
     }
-});
+})
--- content.js
+++ content.js
@@ -1,7 +1,36 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.command=='update') {
-        // 初期化処理
-        var saerch_form = document.getElementById("twotabsearchtextbox");
-        saerch_form.value = "鬼滅の刃"
+    if (request.command=='inputAddress') {
+        // 国/地域自動選択
+        changeList(0, "address-ui-widgets-countryCode-dropdown-nativeId", request.countryCode);
+        // 都道府県自動選択
+        changeList(1, "address-ui-widgets-enterAddressStateOrRegion-dropdown-nativeId", request.region);
+        // テキストボックスへの自動入力
+        document.getElementById("address-ui-widgets-enterAddressFullName").value = request.userName;
+        document.getElementById("address-ui-widgets-enterAddressPostalCodeOne").value = request.postalCodeTop;
+        document.getElementById("address-ui-widgets-enterAddressPostalCodeTwo").value = request.postalCodeUnder;
+        document.getElementById("address-ui-widgets-enterAddressLine1").value = request.addrress1;
+        document.getElementById("address-ui-widgets-enterAddressLine2").value = request.addrress2;
+        document.getElementById("address-ui-widgets-enterAddressLine3").value = request.addrress3;
+        document.getElementById("address-ui-widgets-enterAddressPhoneNumber").value = request.tell;
     }
 });
+
+// プルダウンリスト選択値変更の関数
+function changeList(listNo, selectId, optionName) {
+    if (optionName === null){
+        // 指定された値がない場合、プルダウンの変更は行わず処理終了
+        return;
+    }
+    pullDownList = document.getElementById(selectId);
+    pulldown_option = pullDownList.getElementsByTagName('option');
+    for(i=0; i<pulldown_option.length;i++){
+        if(pulldown_option[i].value == optionName){
+            // プルダウンの選択内容を変更
+            pulldown_option[i].selected = true;
+            // 選択内容を変えても画面表示の国、県名は変わらないため選択されているoption要素のテキストを表示させる
+            selectedItem = document.getElementsByClassName("a-dropdown-prompt");
+            selectedItem[listNo].textContent = pulldown_option[i].textContent
+            break;
+        }
+    }
+}
